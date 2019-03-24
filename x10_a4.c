144317a5c2cbea3f86b04a36ade22cba193cc623
jerryaswell <jerryaswell@hushmail.com>
2019-03-24 19:42:11 -0400

    Refactor assessment 10 question 4
    Reformat whitespace

--- x10_a4.c
+++ x10_a4.c
@@ -2,8 +2,8 @@
 #include<stdio.h>
 
 typedef struct BST_NODE_T {
-  int data;
-  struct BST_NODE_T  *left, *right;
+	int data;
+	struct BST_NODE_T *left, *right;
 } bst_node_t;
 
 int sum(bst_node_t *curr) {
@@ -14,36 +14,37 @@ int sum(bst_node_t *curr) {
 }
 
 int main() {
-  int n;
-  bst_node_t *root = 0;
-  //building  bst from incoming integer data
-  while(1==scanf("%d", &n)) {
-    bst_node_t *nb = malloc(sizeof(bst_node_t));
-    nb->data = n;
-    nb->left=0;
-    nb->right=0;
-    //put the new node into the bst
-    //search for insertion location
-    bst_node_t *curr=root;
-    bst_node_t *prev=0;
-    while(curr!=0) {
-      prev = curr;
-      if (n>curr->data)
-         curr = curr->right;
-      else
-         curr = curr->left;
-    }
-    if (prev==0)   {
-      root = nb;
-    }
-    else {
-       if (n>prev->data)
-          prev->right = nb;
-       else
-          prev->left = nb;
-    }
+	int n;
+	bst_node_t *root = 0;
+	//building bst from incoming integer data
+	while(1==scanf("%d", &n)) {
+		bst_node_t *nb = malloc(sizeof(bst_node_t));
+		nb->data = n;
+		nb->left=0;
+		nb->right=0;
+		//put the new node into the bst
+		//search for insertion location
+		bst_node_t *curr=root;
+		bst_node_t *prev=0;
 
-  }
-  //finished building the tree
-  printf("%d\n",sum(root));
+		while(curr!=0) {
+			prev = curr;
+			if (n>curr->data)
+				 curr = curr->right;
+			else
+				 curr = curr->left;
+		}
+
+		if (prev==0) {
+			root = nb;
+		} else {
+			if (n>prev->data)
+				prev->right = nb;
+		else
+			prev->left = nb;
+		}
+
+	}
+	//finished building the tree
+	printf("%d\n",sum(root));
 }
