60eb7de84b366eb0a12c5926b0e383b8692b8ae9
jerryaswell <jerryaswell@hushmail.com>
2019-03-24 19:25:39 -0400

    Refactor assessment 10 question 3
    Reformat whitespace
    Implement a recursion version of the addOdd function

--- x10_a3.c
+++ x10_a3.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef
-struct NODE{
-  int data;
-  struct NODE *next;
-} node_t;     // node_t is the new data type
+typedef struct NODE {
+	int data;
+	struct NODE *next;
+} node_t;
 
 int addOdd(node_t *curr) {
 	int sum = 0;
@@ -19,19 +18,26 @@ int addOdd(node_t *curr) {
 	return sum;
 }
 
-int main() {
-  int n;
-  //build the list.
-  node_t *head = 0;
-  while (1==scanf("%d", &n) ) {
-     //process the new number;
-        //make a new node   
-        node_t *nb = malloc(sizeof(node_t));
-        //put the data into the node
-        nb->data = n;
-        //put the node into the list
-        nb->next = head;
-        head = nb;
-  }
-  printf("%d\n",addOdd(head));
+int addOddRecursion(node_t *curr) {
+	if (curr == 0) return 0;
+	if (curr->data % 2) return curr->data + addOddRecursion(curr->next);
+	return addOddRecursion(curr->next);
+}
+
+void main() {
+	int n;
+	//build the list.
+	node_t *head = 0;
+	while (1==scanf("%d", &n) ) {
+		//process the new number
+		//make a new node   
+		node_t *nb = malloc(sizeof(node_t));
+		//put the data into the node
+		nb->data = n;
+		//put the node into the list
+		nb->next = head;
+		head = nb;
+	}
+	printf("%d\n",addOdd(head));
+	printf("%d\n",addOddRecursion(head));
 }
