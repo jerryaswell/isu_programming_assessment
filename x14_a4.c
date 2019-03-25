dfd873dfbbf0cf578d71e3c865001685574d347c
jerryaswell <jerryaswell@hushmail.com>
2019-03-24 22:35:32 -0400

    Complete assessment 14 question 4

---
+++ x14_a4.c
@@ -0,0 +1,50 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+typedef struct BST_NODE_T {
+	int data;
+	struct BST_NODE_T *left, *right;
+} bst_node_t;
+
+int child2(bst_node_t *curr) {
+	if (curr == 0) return 0;
+
+	return ((curr->left) && (curr->right)) + child2(curr->left) +
+		child2(curr->right);
+}
+
+int main() {
+	int n;
+	bst_node_t *root = 0;
+	//building bst from incoming integer data
+	while(1==scanf("%d", &n)) {
+		bst_node_t *nb = malloc(sizeof(bst_node_t));
+		nb->data = n;
+		nb->left=0;
+		nb->right=0;
+		//put the new node into the bst
+		//search for insertion location
+		bst_node_t *curr=root;
+		bst_node_t *prev=0;
+
+		while(curr!=0) {
+			prev = curr;
+			if (n>curr->data)
+				 curr = curr->right;
+			else
+				 curr = curr->left;
+		}
+
+		if (prev==0) {
+			root = nb;
+		} else {
+			if (n>prev->data)
+				prev->right = nb;
+		else
+			prev->left = nb;
+		}
+
+	}
+	//finished building the tree
+	printf("%d\n",child2(root));
+}
