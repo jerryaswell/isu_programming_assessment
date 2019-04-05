4568ad78b2b4ba8a4c7bdaf184ceded4f8a68e7d
jerryaswell <jerryaswell@hushmail.com>
2019-04-05 08:05:13 -0400

    Refactor assessment 7 question 1

--- x07_a1.c
+++ x07_a1.c
@@ -3,18 +3,26 @@
 int main(int argc, char *argv[]){
 	int n;
 
+	// Read input from the user as a decimal
 	scanf("%d", &n);
 
 	int numA = 1;
 	int numB = n;
 
+	// Iterate as many times as specified by the user
 	for (int i=0; i<n; i++) {
+
 		for (int j=0; j<numA; j++)
 			printf("A");
+
 		for (int j=0; j<numB; j++)
 			printf("B");
+
 		printf("C");
+
+		// Print one more 'A' next time
 		numA++;
+		// Print one less 'B' next time
 		numB--;
 	}
 	printf("\n");
